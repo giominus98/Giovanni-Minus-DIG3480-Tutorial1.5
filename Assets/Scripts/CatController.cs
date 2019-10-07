db13b6a5f5f4b3717f367a896d802df5de01c63a
Giovanni Minus <giovanni19minus98@gmail.com>
2019-10-06 23:10:25 -0400

    Tutorial1.5

---
+++ Assets/Scripts/CatController.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CatController : MonoBehaviour
+{
+    public AudioSource MusicSource;
+    public AudioClip MusicClipOne;
+    public AudioClip MusicClipTwo;
+
+    Animator anim;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.W))
+        {
+            MusicSource.clip = MusicClipOne;
+            MusicSource.Play();
+            anim.SetInteger("State", 1);
+        }
+
+        if (Input.GetKeyUp(KeyCode.W))
+        {
+            MusicSource.Stop();
+            anim.SetInteger("State", 0);
+        }
+
+        if (Input.GetKeyDown(KeyCode.R))
+        {
+            MusicSource.clip = MusicClipTwo;
+            MusicSource.Play();
+            anim.SetInteger("State", 2);
+        }
+
+        if (Input.GetKeyUp(KeyCode.R))
+        {
+            MusicSource.Stop();
+            anim.SetInteger("State", 0);
+        }
+
+        if (Input.GetKeyDown(KeyCode.L))
+        {
+            MusicSource.loop = true;
+        }
+
+        if (Input.GetKeyUp(KeyCode.L))
+        {
+            MusicSource.loop = false;
+        }
+
+        if (Input.GetKey("escape"))
+        {
+            Application.Quit();
+        }
+    }
+}
